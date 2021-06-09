2e3f73c3c420f82497786994b7da02e5c765248c
elinformatico <donoe1985@gmail.com>
2021-06-09 10:50:42 -0500

    chore: Agregamos un RequestMapping a nivel de Clase

--- src/main/java/net/elinformatico/controller/CategoriasController.java
+++ src/main/java/net/elinformatico/controller/CategoriasController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping(value = {"/categorias"})
 public class CategoriasController {
 
 	// @GetMapping("/index")
