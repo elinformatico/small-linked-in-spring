62fd4bf66c66e0085afa041d3e5ac35fc4a30f47
elinformatico <donoe1985@gmail.com>
2021-06-09 17:13:08 -0500

    feature: Agregando ejemplo para la anotacion @RequestParam a detalle de la Vacante

--- src/main/java/net/elinformatico/controller/VacantesController.java
+++ src/main/java/net/elinformatico/controller/VacantesController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import net.elinformatico.model.Vacante;
 
@@ -20,10 +21,15 @@ public class VacantesController {
 
 	// @GetMapping("/view/{id}")
 	@RequestMapping(path = {"/view/{id}", "/ver/{id}", "/consultar/{id}"}, method = RequestMethod.GET)
-	public String mostrarVacante(@PathVariable("id") int idVacante, Model model) 
-	{	
+	public String mostrarVacante(
+			@PathVariable("id") int idVacante, 
+			@RequestParam(name = "prop", required = false) String value, 
+			Model model
+	){	
+		// System.out.println("engine: " + engine);
 		model.addAttribute("title", "Mostrar Vacante # " +  idVacante);
 		model.addAttribute("idVacante", idVacante);
+		model.addAttribute("prop", value);
 		return "vacantes/detalle";
 	}
 	
